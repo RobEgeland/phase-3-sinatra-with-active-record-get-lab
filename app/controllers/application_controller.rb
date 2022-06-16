0dca5759bfc89ef7a907e66909e79257fc5036bd
Robert Egeland <robegeland@yahoo.com>
2022-06-15 20:40:44 -0500

    commit tree 86796d427d05c9a9b694eac9fdcf3efee88b6cb7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < Sinatra::Base
 
   # add routes
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do
+    bakeries = Bakery.all 
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do 
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do 
+    goods = BakedGood.all.order(price: :desc)
+    goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do 
+    expensive_good = BakedGood.all.order(price: :desc).limit(1)
+    expensive_good.to_json
+  end
+
+    
 
 end
